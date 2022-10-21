bfb66fa733dc96121b78aaae01c4fb117b47c965
vtvy <votrieuvy1661@gmail.com>
2022-10-21 09:58:58 +0700

    add router

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import AppHeader from "@/components/AppHeader.vue";
+export default {
+  components: {
+    AppHeader,
+  },
+};
+</script>
+<template>
+  <AppHeader />
+  <div class="container mt-3">
+    <router-view />
+  </div>
+</template>
+<style>
+.page {
+  max-width: 400px;
+  margin: auto;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import router from "./router";
+import App from "./App.vue";
+
+createApp(App).use(router).mount("#app");
